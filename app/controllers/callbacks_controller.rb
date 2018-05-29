2c919c949e223cb1c65f42b35903e90162b3c803
Ignacio Parrague <ibparrague@miuandes.cl>
2018-05-29 14:56:05 -0400

    getting 'IPAddr::InvalidAddressError' when loggin in with facebook

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,6 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def omniauth_auth
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_to_redirect @user
+    sign_in_and_redirect@user
   end
 end
--- db/migrate/20180529172653_add_columns_to_users.rb
+++ db/migrate/20180529172653_add_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :name, :string
     add_column :users, :provider, :string
-    add_column :users, :uid, :int
+    add_column :users, :uid, :string
   end
 end
